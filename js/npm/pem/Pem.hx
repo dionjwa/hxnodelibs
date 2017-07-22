a6c0f4418b4fcbcc6dc66703c20236caa6f5e2d5
Dion Amago <dion@transition9.com>
2017-07-21 21:36:35 -0700

    d3, pem, radium

---
+++ js/npm/pem/Pem.hx
@@ -0,0 +1,27 @@
+package js.npm.pem;
+
+import js.Error;
+import express.Middleware;
+import mw.cookieparser.*;
+
+typedef PemCreateCertificationOptions = {
+	@:optional var serviceKey :String;
+	@:optional var serviceKeyPassword :String;
+	@:optional var serviceCertificate :String;
+	@:optional var serial :String;
+	@:optional var selfSigned :Bool;
+	@:optional var days :Int;
+	@:optional var extFile :String;
+	@:optional var config :String;
+}
+
+typedef PemKeys ={
+	var serviceKey :String;
+	var certificate :String;
+}
+
+@:jsRequire("pem")
+extern class Pem
+{
+  @:selfCall static function createCertificate(opts : PemCreateCertificationOptions, cb :Null<Error>->PemKeys->Void) :Void;
+}
