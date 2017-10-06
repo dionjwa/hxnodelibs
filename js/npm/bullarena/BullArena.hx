ab6d5177714d4f7f252815c85114cdaa140181b9
Dion Amago <dion@transition9.com>
2017-10-06 14:41:38 -0700

    bull-arena

---
+++ js/npm/bullarena/BullArena.hx
@@ -0,0 +1,27 @@
+@:jsRequire("bull-arena")
+
+typedef BullArenaOptions = {
+	var queues :Array<BullArenaQueueConfiguration>;
+}
+typedef BullArenaQueueConfiguration = {
+	var name :String;
+	@:optional var port :Int;
+	@:optional var host :String;
+	var hostId :String;
+	@:optional var db :String;
+	@:optional var password :String;
+	@:optional var prefix :String;
+	@:optional var url :String;
+}
+
+typedef BullArenaListenOptions = {
+	@:optional var port :Int;
+	@:optional var basePath :String;
+	@:optional var disableListen :Bool;
+}
+
+extern class BullArena
+{
+	@:selfCall
+	public function new(queueOpts :BullArenaOptions, ?listenOpts :BullArenaListenOptions) :Void;
+}
